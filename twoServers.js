13c89bba63a4250c6e91a7bf0759cd3907834196
Lappy Mc Lap Face <ephillipsiii>
2018-04-23 19:33:08 -0600

    two servers set up, one with a positive message, one with a negative

--- twoServers.js
+++ twoServers.js
@@ -4,20 +4,20 @@ var PORT1 = 7000;
 
 var PORT2 = 7500;
 
-function handleRequest(request, response){
+function handleRequest1(request, response){
     response.end("Your are Awesome!" + request.url);
 }
 
-var server1 = http.createServer(handleRequest);
+var server1 = http.createServer(handleRequest1);
 server1.listen(PORT1, function(){
     console.log("Server listening on http://localhost:" + PORT1);
 });
 
-function handleRequest(request, response){
+function handleRequest2(request, response){
     response.end("You are terrible!" + request.url);
 }
 
-var server2 = http.createServer(handleRequest);
+var server2 = http.createServer(handleRequest2);
 server2.listen(PORT2, function(){
     console.log("Server listening on http://localhost:" + PORT2);
 });
